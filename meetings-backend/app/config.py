f51f9fb950a73ef6a891141fb75a6c0b890e8f5c
Diego Alvarez S <dialvarezs@gmail.com>
2021-12-31 18:37:46 -0300

    Added CORS and another configs

--- meetings-backend/app/config.py
+++ meetings-backend/app/config.py
@@ -2,9 +2,12 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    debug: bool = False
+    debug: bool = True
+    app_name: str
     database_url: str
     secret_key: str
+    root_path: str = "/"
+    allowed_origins: list[str] = ["*"]
 
     class Config:
         env_file = ".env"
--- meetings-backend/app/main.py
+++ meetings-backend/app/main.py
@@ -1,10 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+from app.config import settings
 from app.modules.auth.api import router as auth_router
 from app.modules.meetings.api import router as meetings_router
 
-app = FastAPI()
+if not settings.debug:
+    docs_opts = {"docs_url": None, "redoc_url": None}
+else:
+    docs_opts = {}
+
+app = FastAPI(title=settings.app_name, root_path=settings.root_path, **docs_opts)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins,
+    allow_methods=["GET", "POST", "PATCH"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(meetings_router, prefix="/meetings", tags=["meetings"])
